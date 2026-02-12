f80907c059a05c1122e80e68391a07420a1788dc
Jorge A. Lopez <Jorgelopez5252@gmail.com>
2026-02-11 23:53:41 -0700

    final update for the night till tommorow

---
+++ src/components/StarBackground.jsx
@@ -0,0 +1,15 @@
+import {useState} from "react";
+
+//id, size, y,x opacity and animation duration
+
+
+export const StarBackground = () => { 
+    const [stars, setStars] = useState([])
+
+    const generateStars =() => {
+        const numberOfStar = Math.floor(window.innerWidth * window.innerHeight / 10000)
+    }
+    return <div></div> 
+};
+
+// 50:09
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -5,3 +5,4 @@ export const cn = (...inputs) => {
 return twMerge (clsx(inputs));
 };
 
+
