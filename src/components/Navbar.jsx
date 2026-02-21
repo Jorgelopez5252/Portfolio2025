2a9365c383854102771a18106ece6bd3a4328f7e
Jorge A. Lopez <Jorgelopez5252@gmail.com>
2026-02-21 00:23:27 -0700

    added ui elements and navebar completed getting ready for core layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@
 // import { useState } from "react";
 import { cn } from "@/lib/utils";
 import { useEffect, useState } from "react";
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
 
 
 const navItems = [
@@ -37,6 +37,7 @@ export const Navbar = () => {
                 <a className="text-xl font-bold text-primary flex items-center"
                     href="#hero">
                     <span className="relative z-10">
+                        {/* maybe add glow to my name? */}
                         <span className="text-glow text-foreground">Jorge Lopez </span>
                         Portfolio
                     </span>
@@ -51,7 +52,10 @@ export const Navbar = () => {
                     ))}
                 </div>
                 {/* Mobile Menu Nav */}
-                    <button> {isMenuOpen? <X size={24} /> : <Menu size={24}/>}</button>
+                    <button onClick={() => setIsMenuOpen((prev) => !prev)} className="md:hidden p-2 text-foreground z-50" aria-label = {isMenuOpen ? "Close menu" : "Open menu"}>
+                         {isMenuOpen? <X size={24} /> : <Menu size={24}/>}
+                    
+                    </button>
 
                 <div className={cn("fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center ", "transition-all duration-300 md:hidden", isMenuOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none")}>
                     <div className="flex flex-col space-y-8 text-xl">
